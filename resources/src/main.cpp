efe0c3ba5fedd5ddcb9c7cb03e2c7aba32b2e009
Alex De Luera <134332668+alexdeluera@users.noreply.github.com>
2025-04-20 20:02:09 -0400

    GUI version of main
    main updated to reflect the version of the GUI that will be used in the demo video

--- resources/src/main.cpp
+++ resources/src/main.cpp
@@ -1,54 +1,120 @@
-#include <iostream>
-#include <vector>
+#include <QApplication>
+#include <QWidget>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QFormLayout>
+#include <QDir>
 #include <chrono>
-#include <iomanip>
+
+#include <iostream>
+
 #include "City.h"
 #include "MaxHeap.h"
 #include "MinHeap.h"
+
 using namespace std;
 using namespace std::chrono;
 
-int main() {
+class InputWindow : public QWidget {
+public:
+    InputWindow(QWidget *parent = nullptr);
 
-    //getting user input for choice of country
-    string country;
-    cout<<"Enter country of choice: "<<endl;
-    getline(cin, country);
+private slots:
+    void onSubmit();
 
-    //getting user input for population preference
-    int population_preference;
-    cout<<"Enter population preference from a scale of 1-5: 1 being least populous and 5 being most populous"<<endl;
-    cin>>population_preference;
+private:
+    QLineEdit *inputField1;
+    QLineEdit *inputField2;
+    QLineEdit *inputField3;
+    QLabel *resultLabel;
+};
 
-    //getting user input for weather preference
-    int weather_preference;
-    cout<<"Enter weather preference from scale of 1-5: 1 being coldest and 5 being warmest"<<endl;
-    cin>>weather_preference;
+InputWindow::InputWindow(QWidget *parent) : QWidget(parent) {
+    setWindowTitle("City Selector");
 
-    vector<City> cities = City::getInfo("../worldcities.csv", country, population_preference, weather_preference);
+    inputField1 = new QLineEdit(this);
+    inputField2 = new QLineEdit(this);
+    inputField3 = new QLineEdit(this);
 
-    cout<<"---------------------MaxHeap Implementation results------------------------"<<": "<<endl;
+    QPushButton *submitButton = new QPushButton("Submit", this);
+
+    resultLabel = new QLabel(this);
+    resultLabel->setText("Results will be displayed here.");
+
+    QLabel *instructionLabel = new QLabel("Please enter inputs below:", this);
+    instructionLabel->setAlignment(Qt::AlignLeft);  // Center the text
+
+    QVBoxLayout *layout = new QVBoxLayout(this);
+
+    QFormLayout *formLayout = new QFormLayout();
+    formLayout->addRow("Country:", inputField1);
+    formLayout->addRow("Population, Integer 1-5: ", inputField2);
+    formLayout->addRow("Weather, Integer 1-5: ", inputField3);
+
+    layout->addWidget(instructionLabel);
+    layout->addLayout(formLayout);
+    layout->addWidget(submitButton);
+    layout->addWidget(resultLabel);
+
+    connect(submitButton, &QPushButton::clicked, this, &InputWindow::onSubmit);
+
+    setLayout(layout);
+}
+
+void InputWindow::onSubmit() {
+    QString country = inputField1->text();
+    QString population = inputField2->text();
+    QString weather = inputField3->text();
+
+    string countryStr = country.toStdString();
+    int populationInt = population.toInt();
+    int weatherInt = weather.toInt();
+
+    qDebug() << "QDir::currentPath(): "<<QDir::currentPath();
+    qDebug() << "QApplication::: "<<QApplication::applicationDirPath();
+    qDebug() << "QCoreApplication::: "<<QCoreApplication::applicationDirPath();
+
+    vector<City> cities = City::getInfo("worldcities.csv", countryStr, populationInt, weatherInt);
+
     auto start_timer_maxHeap = high_resolution_clock::now();
-
     MaxHeap maxheap(cities);
-    maxheap.top_5_cities();
+    string maxOutput = maxheap.top_5_str();
     auto end_timer_maxHeap = high_resolution_clock::now();
 
     auto maxHeap_runtime = duration_cast<duration<double, milli>>(end_timer_maxHeap - start_timer_maxHeap);
     double maxHeap_run = maxHeap_runtime.count();
 
-    cout<<"MaxHeap execution time: "<< fixed<<setprecision(3)<<maxHeap_run<< " milliseconds"<<endl;
+    stringstream max_ss;
+    max_ss << "MaxHeap execution time: " << std::fixed << std::setprecision(3) << maxHeap_run << " milliseconds. \n \n";
+    string maxRunTime = max_ss.str();
 
-    cout<<endl<<"---------------------MinHeap Implementation results------------------------"<<": "<<endl;
     auto start_timer_minHeap = high_resolution_clock::now();
     MinHeap minHeap(cities);
-    minHeap.top_5_cities();
+    string minOutput = minHeap.top_5_str();
     auto end_timer_minHeap = high_resolution_clock::now();
 
     auto minHeap_runtime = duration_cast<duration<double, milli>>(end_timer_minHeap - start_timer_minHeap);
     double minHeap_run = minHeap_runtime.count();
 
-    cout<<"MinHeap execution time: "<< fixed<<setprecision(3)<<minHeap_run<< " milliseconds"<<endl <<endl;
+    stringstream min_ss;
+    min_ss << "MinHeap execution time: " << std::fixed << std::setprecision(3) << minHeap_run << " milliseconds. \n";
+    string minRunTime = min_ss.str();
 
-    return 0;
+    QString output = QString::fromStdString(maxOutput);
+
+    output+=maxRunTime+minOutput+minRunTime;
+
+    resultLabel->setText(output);
+}
+
+int main(int argc, char *argv[]) {
+
+    QApplication app(argc, argv);
+
+    InputWindow window;
+    window.show();
+
+    return app.exec();
 }
